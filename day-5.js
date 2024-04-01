861f5e033c94af52da3c63ec519010438b75d198
cyber-mannu <sahanimannu844@gmail.com>
2024-04-01 09:33:19 +0530

    day 5 is done

---
+++ day-5.js
@@ -0,0 +1,6 @@
+const arry = [1, 8, 4, 6, 3,]
+const shortedNumber = arry.sort((a,b)=> a-b)
+console.log(shortedNumber);
+
+
+// this is how we can sort  any numbers in an  array  with in build function sort ()  method
