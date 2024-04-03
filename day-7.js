3f461487cac1bc032db4b0d3c92a4f123a8c1e4b
cyber-mannu <sahanimannu844@gmail.com>
2024-04-03 19:48:10 +0530

    day 7 is done

---
+++ day-7.js
@@ -0,0 +1,22 @@
+const numbers = [2, 6, 1, 5, -1, 4, 9]; // finding the max between an array with math.max() method ..
+
+
+
+const maxfind = Math.min(...numbers)
+console.log(maxfind);
+
+
+
+// const max =  numbers.sort((a, b )=>a-b)
+// console.log(max); // we can also find this using sort() method 
+
+
+
+//  numbers.reduce((acc, acctualval)=>{
+
+// if(acctualval<numbers){
+// }
+
+// return acc
+
+// }, 0)
