bed45741e9146e93100ee0df3981b99a08eadaa3
cyber-mannu <sahanimannu844@gmail.com>
2024-04-04 19:56:47 +0530

    uncomment

--- day-9.js
+++ day-9.js
@@ -1,11 +1,12 @@
-// const numbers = (str) => {
-//   let total = str.reduce((accumalive, acctualVal) => acctualVal + accumalive);
-//   const average = total / str.length;
-//   console.log(average);
-// };
+const numbers = (str) => {
+  let total = str.reduce((accumalive, acctualVal) => acctualVal + accumalive);
+  const average = total / str.length;
+  console.log(average);
+};
 
-// console.log(numbers([1, 4, 5, 9, 45]));
+console.log(numbers([1, 4, 5, 9, 45]));
 
+// here i just learned how to use reduce method in an array to find out the sum of that array and average of that arrya
 
 
 
