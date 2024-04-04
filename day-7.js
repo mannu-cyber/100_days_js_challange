90ad2573394ec4e68a927c4248a67cac3e8522bf
cyber-mannu <sahanimannu844@gmail.com>
2024-04-04 08:02:32 +0530

    day 8 done

--- day-7.js
+++ day-7.js
@@ -1,16 +1,10 @@
-const numbers = [2, 6, 1, 5, -1, 4, 9]; // finding the max between an array with math.max() method ..
+// const numbers = [2, 6, 1, 5, -1, 4, 9]; // finding the max between an array with math.max() method ..
 
-
-
-const maxfind = Math.min(...numbers)
-console.log(maxfind);
-
-
+// const maxfind = Math.min(...numbers)
+// console.log(maxfind);
 
 // const max =  numbers.sort((a, b )=>a-b)
-// console.log(max); // we can also find this using sort() method 
-
-
+// console.log(max); // we can also find this using sort() method
 
 //  numbers.reduce((acc, acctualval)=>{
 
---
+++ day-8.js
@@ -0,0 +1,48 @@
+// const factorial = (str) => {
+//   let fact = 1;
+//   for (let i = 1; i <= str; i++) {
+//     fact *= i;
+//   }
+//   return fact;
+// };
+
+// console.log(factorial(0));
+
+// console.log(4*3*2*1);
+
+// const factor = (str) => {
+//   let findingFact = 1;
+//   for (let i = 1; i <= str; i++) {
+//     findingFact *= i;
+//   }
+//   return findingFact;
+// };
+
+// console.log(factor(11));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const findingFack = (str)=>{
+
+
+//        let fact = 1
+//        for (let i = 1 ; i<=str; i++){
+
+// fact*=i
+
+//        }
+//        return fact
+// }
+
+// console.log(findingFack(3));
