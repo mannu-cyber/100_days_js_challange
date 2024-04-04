33ea8706827cc42d55b083312d6be9fa081fb06b
cyber-mannu <sahanimannu844@gmail.com>
2024-04-04 19:55:21 +0530

    day 9 done

---
+++ day-9.js
@@ -0,0 +1,12 @@
+// const numbers = (str) => {
+//   let total = str.reduce((accumalive, acctualVal) => acctualVal + accumalive);
+//   const average = total / str.length;
+//   console.log(average);
+// };
+
+// console.log(numbers([1, 4, 5, 9, 45]));
+
+
+
+
+
