e1c58708a3f5e7bc5d663ec2aa89152385b1f59a
cyber-mannu <sahanimannu844@gmail.com>
2024-04-06 09:22:24 +0530

    day 10 done and with that i learned every() method in js

---
+++ day-10.js
@@ -0,0 +1,31 @@
+// const comparing = (ar1, ar2) => {
+//   if (ar1.length === ar2.length) {
+//     // return true
+
+//     for (let i = 0; i < ar1.length; i++) {
+//       if (ar1[i] !== ar2[i]) {
+//         return false;
+//       } else {
+//         return true;
+//       }
+//     }
+//   } else {
+//     return false;
+//   }
+// };
+
+const comparing = (arr1, arr2) => {
+  if (arr1.length === arr2.length) {
+    return arr1.every((curval, index) => curval=== arr2[index]); // every()  method is used to check all the element in an arrya are satisfying the particular block of logic or not  
+  }
+else{
+     return  false
+}
+
+};
+
+const arr1 = [2, 4, 65, 65, 3, 23 ,3];
+
+const arr2 = [2, 4, 65, 65, 3, 23];
+
+console.log(comparing(arr1, arr2));
